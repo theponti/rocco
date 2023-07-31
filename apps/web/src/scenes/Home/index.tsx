5ac240bac6804bea7a34cfa6f58d33e432506fe8
charlesponti <cj@ponti.io>
2023-07-31 12:01:19 -0700

    feat(apps/web): update style of landing page

--- apps/web/src/scenes/Home/index.tsx
+++ apps/web/src/scenes/Home/index.tsx
@@ -1,6 +1,5 @@
 import styled from "@emotion/styled";
 
-import { APP_NAME } from "src/constants";
 import Emoji from "ui/Emoji";
 
 import svg from "./world.svg";
@@ -8,10 +7,14 @@ import { mediaQueries } from "src/constants/styles";
 
 const Wrap = styled.div`
   display: flex;
-  flex-direction: column;
   justify-items: center;
   align-items: center;
+  margin-bottom: 4rem;
   width: 100%;
+
+  ${mediaQueries.belowMedium} {
+    flex-direction: column;
+  }
 
   ${mediaQueries.belowMedium} {
     max-width: calc(100% - 16px);
@@ -19,22 +22,8 @@ const Wrap = styled.div`
   }
 `;
 
-const Hero = styled.div`
-  display: flex;
-  justify-items: center;
-  align-items: center;
-  width: 100%;
-  height: 80vh;
-  margin-bottom: 4rem;
-
-  ${mediaQueries.belowMedium} {
-    flex-direction: column;
-  }
-`;
-
 const HeroImage = styled.div`
   width: 50%;
-  height: 100%;
   background-image: url(${svg});
   background-size: cover;
   background-position: center;
@@ -61,45 +50,32 @@ const HeroText = styled.div`
 const Home = () => {
   return (
     <Wrap>
-      <Hero>
-        <HeroImage />
-        <HeroText>
-          <h1 className="text-base-content text-[6rem] font-extrabold">
-            {APP_NAME}
-          </h1>
-
-          <p className="text-neutral-content font-extrabold text-xl">
-            go more places
-          </p>
-
-          <div className="mt-8">
-            <button className="btn btn-secondary text-primary font-extrabold lowercase">
-              Get Started
-            </button>
-          </div>
-        </HeroText>
-      </Hero>
-
-      <div className="flex justify-between items-center mt-16 mb-16 gap-14 max-md:flex-col h-20">
-        <p className="flex items-center text-neutral-content font-extrabold text-xl">
+      <HeroImage className="h-52 md:h-full" />
+      <HeroText className="flex justify-between items-center mt-16 mb-16 gap-14 max-md:flex-col w-44">
+        <p className="flex items-center text-primary font-extrabold text-xl">
           <Emoji kind="map" size="lg" className="animate-spin-slow">
             🌎
           </Emoji>
-          <span className="ml-2">Explore the world</span>
+          <span className="ml-2">the world if finally yours</span>
         </p>
-        <p className="flex items-center text-neutral-content font-extrabold text-xl">
+        <p className="flex items-center text-primary font-extrabold text-xl">
           <Emoji kind="map" size="lg" className="animate-spin-slow">
-            🌍
+            📍
           </Emoji>
-          <span className="ml-2">Discover new places</span>
+          <span className="ml-2">Discover the places your friends love</span>
         </p>
-        <p className="flex items-center text-neutral-content font-extrabold text-xl">
+        <p className="flex items-center text-primary font-extrabold text-xl">
           <Emoji kind="map" size="lg" className="animate-spin-slow">
-            🌏
+            🤩
           </Emoji>
-          <span className="ml-2">Meet new people</span>
+          <span className="ml-2">Never wonder where to go again</span>
         </p>
-      </div>
+        <div className="mt-2">
+          <button className="btn btn-primary font-extrabold lowercase">
+            Get Started
+          </button>
+        </div>
+      </HeroText>
     </Wrap>
   );
 };
