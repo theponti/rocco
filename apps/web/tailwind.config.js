499fc5372909740aaefb341c5f02a4c5063c9884
charlesponti <cj@ponti.io>
2023-07-30 18:27:25 -0700

    feat(apps/web): add spin-slow animation to tailwind

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "spin-slow": "spin 6s linear infinite",
+      },
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
