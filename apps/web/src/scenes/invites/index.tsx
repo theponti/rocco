644a0f38d28dbbd32124d0d395e2cf07b465524a
Charles Ponti <cj@ponti.io>
2023-12-10 19:43:54 -0800

    feat: empty invites web route

--- apps/web/src/scenes/invites/index.tsx
+++ apps/web/src/scenes/invites/index.tsx
@@ -1,17 +1,12 @@
 import { useNavigate } from "react-router-dom";
-import LoadingScene from "ui/Loading";
 
 import DashboardWrap from "src/components/DashboardWrap";
-import { useListInvites } from "src/services/api";
 import { useAppSelector } from "src/services/hooks";
 import { getUser } from "src/services/store";
 
-import ListInviteItem from "./components/ListInviteItem";
-
-const ListInvites = () => {
+const Invites = () => {
   const navigate = useNavigate();
   const user = useAppSelector(getUser);
-  const { data, refetch, status } = useListInvites();
 
   if (!user) {
     navigate("/");
@@ -19,27 +14,9 @@ const ListInvites = () => {
 
   return (
     <DashboardWrap>
-      <h1>List Invites</h1>
-      {status === "loading" && <LoadingScene />}
-      {status === "success" && (
-        <div>
-          {data?.length === 0 &&
-            "Invites others have sent you will appear here."}
-          {data && data.length > 0 && (
-            <ul className="space-y-2">
-              {data.map((invite) => (
-                <ListInviteItem
-                  key={invite.listId}
-                  invite={invite}
-                  onAcceptInvite={refetch}
-                />
-              ))}
-            </ul>
-          )}
-        </div>
-      )}
+      <h1>Invites</h1>
     </DashboardWrap>
   );
 };
 
-export default ListInvites;
+export default Invites;
