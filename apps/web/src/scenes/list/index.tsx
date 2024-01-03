f94d71360143dbdf8e43acc32bc4ae146b640ab7
Charles Ponti <cj@ponti.io>
2024-01-02 18:35:33 -0800

    Update list scene UI

--- apps/web/src/scenes/list/index.tsx
+++ apps/web/src/scenes/list/index.tsx
@@ -30,11 +30,15 @@ const List = () => {
       {data && (
         <div className="flex flex-col px-0.5">
           <div className="flex justify-between items-center mb-8">
-            <h1 className="text-3xl font-semibold">{data.name}</h1>
+            <h1 className="text-3xl font-semibold text-white">{data.name}</h1>
             {/* Only list owners can invite others. */}
             {data.userId === user.id && (
-              <Link to={`/lists/${data.id}/invites`}>
-                <span className="text-lg hover:cursor-pointer text-primary">
+              <Link
+                to={`/lists/${data.id}/invites`}
+                className="flex gap-2 text-primary btn btn-sm btn-neutral"
+              >
+                Invite others
+                <span className="text-lg hover:cursor-pointer">
                   <UserPlus />
                 </span>
               </Link>
@@ -43,16 +47,18 @@ const List = () => {
           {data.items.map((place) => (
             <div
               key={place.id}
-              className="card p-2 py-3 rounded-md flex flex-col bg-primary text-primary-content mb-10"
+              className="card p-2 py-3 rounded-md flex flex-col bg-neutral text-primary mb-10"
             >
-              <span className="mb-1 text-lg font-medium">{place.name}</span>
+              <span className="mb-2 text-lg font-semibold uppercase">
+                {place.name}
+              </span>
               <div className="flex gap-2">
                 {place.types.map((type) => (
                   <span
                     key={type}
-                    className="bg-black rounded px-2 py-1 text-white text-sm"
+                    className="bg-black rounded px-2 py-1 text-white text-xs capitalize"
                   >
-                    {type}
+                    {type.replace(/_/gi, " ")}
                   </span>
                 ))}
               </div>
