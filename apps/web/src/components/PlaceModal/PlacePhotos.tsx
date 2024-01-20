4c7835ef679ab5311a4a278e442e96b7978fe3ba
Charles Ponti <cj@ponti.io>
2024-01-20 15:58:33 -0800

    Refactor PlacePhotos component to improve image display

--- apps/web/src/components/PlaceModal/PlacePhotos.tsx
+++ apps/web/src/components/PlaceModal/PlacePhotos.tsx
@@ -10,8 +10,12 @@ const PlacePhotos = ({ alt, photos }: Props) => {
   return (
     <div className="carousel carousel-center p-4 space-x-4 bg-slate-200 rounded-box h-64">
       {photos.map((photo) => (
-        <div key={photo.getUrl()} className="carousel-item">
-          <img src={photo.getUrl()} alt={alt} className="rounded-box" />
+        <div key={photo.getUrl()} className="carousel-item max-w-[75%]">
+          <img
+            src={photo.getUrl()}
+            alt={alt}
+            className="rounded-box object-cover"
+          />
         </div>
       ))}
     </div>
