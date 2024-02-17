be7477b713988c06d326e7803b3079828815a587
Charles Ponti <charlesponti@users.noreply.github.com>
2024-02-17 23:27:48 +0000

    [PON-70] feat: Improve product navigation (#50)
    * feat(apps/web): add top nav icons
    
    * feat(apps/web): add bottom navigation

--- apps/web/src/scenes/invites/index.tsx
+++ apps/web/src/scenes/invites/index.tsx
@@ -1,5 +1,4 @@
 import { useNavigate } from "react-router-dom";
-import Typography from "ui/Typography";
 
 import { useGetInvites } from "src/services/api";
 import { useAuth } from "src/services/store";
@@ -16,9 +15,7 @@ const Invites = () => {
 
   return (
     <>
-      <Typography className="mb-4" variant="h1">
-        Invites
-      </Typography>
+      <h1 className="text-4xl mb-4">Invites</h1>
       {invites && (
         <ul className="space-y-4">
           {invites.map((listInvite) => (
