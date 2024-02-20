8b13ece409ed83589d27c33c60ced637ee25cf10
Charles Ponti <cj@ponti.io>
2024-02-19 17:54:11 -0800

    fix(app/api): update place photo cron to use correct value

--- apps/api/src/plugins/lists/get/list/index.ts
+++ apps/api/src/plugins/lists/get/list/index.ts
@@ -3,7 +3,6 @@ import { FastifyInstance } from "fastify";
 // src/plugins
 import { verifySession } from "../../../auth";
 import { prisma } from "../../../prisma";
-import { getPlacePhoto } from "../../../google";
 
 async function getListPlaces(listId: string): Promise<
   {
@@ -92,12 +91,7 @@ const getListRoute = (server: FastifyInstance) => {
 
       const items = await getListPlaces(id);
 
-      return {
-        ...list,
-        items,
-        userId: list.userId,
-        photo: await getPlacePhoto(items[0].googleMapsId),
-      };
+      return { ...list, items, userId: list.userId };
     },
   );
 };
