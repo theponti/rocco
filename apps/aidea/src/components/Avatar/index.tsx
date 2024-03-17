9f4309753258a8b3eca396272b58cfae97e50412
Charles Ponti <cj@ponti.io>
2024-03-17 11:03:59 +1100

    Refactor Avatar component to remove unused prop

--- apps/aidea/src/components/Avatar/index.tsx
+++ apps/aidea/src/components/Avatar/index.tsx
@@ -1,22 +1,20 @@
 import * as RadixAvatar from "@radix-ui/react-avatar";
 import { PersonIcon } from "@radix-ui/react-icons";
-import { ReactNode } from "react";
 import styles from "./Avatar.module.css";
 
 type AvatarProps = {
   alt: string;
   delayMs?: number;
-  fallback?: ReactNode;
   src: string;
 };
-const Avatar = ({ alt, delayMs, fallback, src }: AvatarProps) => (
+const Avatar = ({ alt, delayMs, src }: AvatarProps) => (
   <RadixAvatar.Root className={styles.AvatarRoot}>
     <RadixAvatar.Image className={styles.AvatarImage} src={src} alt={alt} />
     <RadixAvatar.Fallback
       className={styles.AvatarFallback}
       delayMs={delayMs ?? 600}
     >
-      {fallback ?? <PersonIcon />}
+      <PersonIcon />
     </RadixAvatar.Fallback>
   </RadixAvatar.Root>
 );
