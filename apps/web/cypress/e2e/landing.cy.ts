ca64dfc15b97704701782931c9b935f3d31e3c80
Charles Ponti <cj@ponti.io>
2024-03-24 15:27:04 +1100

    Refactor test description in landing.cy.ts

--- apps/web/cypress/e2e/landing.cy.ts
+++ apps/web/cypress/e2e/landing.cy.ts
@@ -1,5 +1,5 @@
 describe("landing flow", () => {
-  test("should load", () => {
+  it("should load", () => {
     cy.visit("http://localhost:53422");
     cy.contains("Make the world yours").should("be.visible");
   });
