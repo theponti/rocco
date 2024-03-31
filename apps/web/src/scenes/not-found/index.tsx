c810ee0e65490adbc79ba8f7954f56ab0fbd1624
Charles Ponti <cj@ponti.io>
2024-03-30 21:19:07 -0700

    feat(apps/web): add not found for list

--- apps/web/src/scenes/not-found/index.tsx
+++ apps/web/src/scenes/not-found/index.tsx
@@ -1,12 +1,22 @@
-import { FileX2 } from "lucide-react";
-import { Link, useLocation } from "react-router-dom";
+import { FileX2, SearchX } from "lucide-react";
+import { PropsWithChildren } from "react";
+import { Link, useLocation, useMatch } from "react-router-dom";
+
+const Wrapper = ({ children }: PropsWithChildren) => {
+  return (
+    <div className="flex flex-col gap-4 w-full justify-center items-center">
+      {children}
+    </div>
+  );
+};
 
 function NotFound() {
+  const isListRoute = useMatch("/list/:id");
   const pathname = useLocation().pathname;
 
   if (pathname.indexOf("invites") !== -1) {
     return (
-      <div className="py-2">
+      <Wrapper>
         <FileX2 size={100} className="text-slate-700" />
         <h2 className="text-2xl font-semibold">
           This invite could not be found.
@@ -18,12 +28,33 @@ function NotFound() {
           </Link>{" "}
           to view your new list.
         </p>
-      </div>
+      </Wrapper>
+    );
+  }
+
+  if (isListRoute) {
+    return (
+      <Wrapper>
+        <FileX2 size={100} className="text-slate-700" />
+
+        <h2 className="text-2xl font-semibold">
+          This list could not be found.
+        </h2>
+
+        <p className="text-md">
+          If you think this is a mistake,{" "}
+          <Link to="/login" className="text-blue-400 font-semibold">
+            log in
+          </Link>{" "}
+          to view your lists.
+        </p>
+      </Wrapper>
     );
   }
 
   return (
-    <div className="flex flex-col w-full h-full justify-center items-center">
+    <Wrapper>
+      <SearchX size={100} className="text-slate-700" />
       <h1 className="text-3xl font-semibold mb-16">You seem to be lost!</h1>
       <Link
         to="/"
@@ -31,7 +62,7 @@ function NotFound() {
       >
         Go home
       </Link>
-    </div>
+    </Wrapper>
   );
 }
 
