fbc172ef23b3f7e3f3e14881e1fa835009652700
Charles Ponti <cj@ponti.io>
2024-04-07 20:36:54 -0700

    feat(apps/web): add build analyze script

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,3 +1,4 @@
+import { writeFile, writeFileSync } from "node:fs";
 import * as path from "node:path";
 import react from "@vitejs/plugin-react";
 import analyze from "rollup-plugin-analyzer";
@@ -23,6 +24,11 @@ export default defineConfig({
 			plugins: [
 				analyze({
 					summaryOnly: true,
+					writeTo(analysisString) {
+						if (process.env.ANALYZE === "true") {
+							writeFileSync("build-analysis.txt", analysisString);
+						}
+					},
 				}),
 			],
 		},
