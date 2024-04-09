ffc810640454816e0cef394723285c56f4ac7c66
Charles Ponti <cj@ponti.io>
2024-04-08 19:06:22 -0700

    fix(apps/api): disallow wrap in PlaceType

--- apps/web/src/components/places/PlaceType.tsx
+++ apps/web/src/components/places/PlaceType.tsx
@@ -7,7 +7,7 @@ const PlaceType = ({
 	return (
 		<span
 			data-testid="place-type"
-			className={`rounded px-3 py-1 border border-slate-200 text-slate-400 text-xs capitalize italic ${className}`}
+			className={`rounded text-nowrap px-3 py-1 border border-slate-200 text-slate-400 text-xs capitalize italic ${className}`}
 		>
 			{children}
 		</span>
