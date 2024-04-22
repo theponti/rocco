9930d3be0d790225684d49b20a3c6b1e2aa4f8b7
Charles Ponti <cj@ponti.io>
2024-04-22 11:06:22 -0700

    feat(apps/api): use cookie domain for api

--- apps/api/src/plugins/session.ts
+++ apps/api/src/plugins/session.ts
@@ -1,19 +1,17 @@
+import assert from "node:assert";
 import type { FastifyPluginAsync } from "fastify";
 import fp from "fastify-plugin";
 
-const { COOKIE_NAME, COOKIE_SECRET, COOKIE_SALT } = process.env;
+const { COOKIE_DOMAIN, COOKIE_NAME, COOKIE_SECRET, COOKIE_SALT } = process.env;
 
-if (!COOKIE_SECRET) {
-	console.log(
-		"warn",
-		"The COOKIE_SECRET env var is not set. This is unsafe! If running in production, set it.",
-	);
-	throw Error;
-}
+assert(COOKIE_DOMAIN, "The COOKIE_DOMAIN is missing.");
+assert(COOKIE_NAME, "The COOKIE_NAME is missing.");
+assert(COOKIE_SECRET, "The COOKIE_SECRET is missing.");
+assert(COOKIE_SALT, "The COOKIE_SALT is missing.");
 
 const sessionPlugin: FastifyPluginAsync = async (server) => {
 	server.register(require("@fastify/secure-session"), {
-		cookieName: COOKIE_NAME, // the name of the session cookie, defaults to 'session'
+		cookieName: COOKIE_NAME, // Defaults to `session`
 		secret: COOKIE_SECRET,
 		salt: COOKIE_SALT,
 		// options for setCookie, see https://github.com/fastify/fastify-cookie
@@ -23,6 +21,7 @@ const sessionPlugin: FastifyPluginAsync = async (server) => {
 		// from the UI domain so we can safely set these values.
 		cookie: {
 			maxAge: 60 * 60 * 24 * 7,
+			domain: COOKIE_DOMAIN,
 			httpOnly: true,
 			sameSite: "lax",
 			secure: true,
