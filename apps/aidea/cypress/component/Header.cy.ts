1bae093594ce5d60754363b2b6412959de178cf9
Charles Ponti <cj@ponti.io>
2024-04-27 19:13:32 -0700

    chore(apps/aidea): remove aidea

--- apps/aidea/cypress/component/Header.cy.ts
+++
@@ -1,5 +0,0 @@
-describe("Header.cy.ts", () => {
-  it("Header", () => {
-    // cy.mount(<Header />);
-  });
-});
--- apps/aidea/cypress/e2e/landing.cy.ts
+++
@@ -1,6 +0,0 @@
-describe("landing flow", () => {
-  it("should load", () => {
-    cy.visit("http://127.0.0.1:4173");
-    cy.get("[data-testid='LandingHeader']").should("be.visible");
-  });
-});
