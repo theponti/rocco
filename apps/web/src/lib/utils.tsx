285959207b0e698f6ce45863cc71a09cc5405ff2
Charles Ponti <cj@ponti.io>
2024-05-13 13:55:23 -0700

    feat(apps/web): display loading while auth status is loading

--- apps/web/src/lib/utils.tsx
+++ apps/web/src/lib/utils.tsx
@@ -1,8 +1,10 @@
+import Loading from "@hominem/components/Loading";
 import { type ClassValue, clsx } from "clsx";
 import { useEffect } from "react";
 import { type Navigate, useNavigate } from "react-router-dom";
 import { twMerge } from "tailwind-merge";
 import { useAuth } from "./auth";
+import { AuthStatus } from "./auth/types";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
@@ -19,8 +21,12 @@ export function withAuth(Component: React.ComponentType) {
 			}
 		}, [user, navigate]);
 
-		if (status !== "authenticated") {
-			return null;
+		if (status === AuthStatus.Unloaded) {
+			return (
+				<div className="flex w-full max-h-[300px] justify-center items-center">
+					<Loading size="xl" />
+				</div>
+			);
 		}
 
 		return <Component {...props} />;
