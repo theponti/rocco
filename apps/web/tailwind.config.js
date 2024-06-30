0221a63eca9754637ead7ba45c38e00a5ea8ae0f
Charles Ponti <cj@ponti.io>
2024-06-29 17:48:22 -0700

    feat: add animate-spin

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -68,6 +68,7 @@ module.exports = {
 				},
 			},
 			animation: {
+				spin: "spin 3s linear infinite",
 				"spin-slow": "spin 6s linear infinite",
 				"accordion-down": "accordion-down 0.2s ease-out",
 				"accordion-up": "accordion-up 0.2s ease-out",
