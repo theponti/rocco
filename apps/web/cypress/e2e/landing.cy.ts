0402d357503544c63815e01647a29acfbf26963c
Charles Ponti <cj@ponti.io>
2025-05-08 14:42:47 -0700

    refactor: update localhost port in Cypress tests and package.json

--- apps/web/cypress/e2e/landing.cy.ts
+++ apps/web/cypress/e2e/landing.cy.ts
@@ -1,6 +1,6 @@
 describe("landing flow", () => {
 	it("should load", () => {
-		cy.visit("http://localhost:53422");
+		cy.visit("http://localhost:3000");
 		cy.contains("All in one place.").should("be.visible");
 	});
 });
