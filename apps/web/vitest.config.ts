7819e42b590fe5ee665a9a9be66725d4b60f2630
Charles Ponti <cj@ponti.io>
2025-05-08 14:19:13 -0700

    refactor: remove vite-plugin-pwa dependency from package.json and update yarn.lock

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -3,6 +3,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+	// @ts-ignore
 	plugins: [react(), tsconfigPaths()],
 	test: {
 		globals: true,
